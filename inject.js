9ee5024e835879c9d869b8ecac553731d52c9d2c
dedol <dedol@ro.ru>
2021-05-09 10:58:21 +0400

    fix BigInt error

--- inject.js
+++ inject.js
@@ -1,26 +1,26 @@
 let productPrice = document.querySelector('.product-price-current');
 if (productPrice) {
-    button = document.createElement('button');
+    let button = document.createElement('button');
     button.setAttribute('type', 'button');
     button.setAttribute('id', 'user-copy-data');
     button.setAttribute('class', 'next-btn next-small next-btn-primary');
     button.innerHTML = 'Copy Data';
-    priceWrapper = document.querySelector('.product-price');
+    let priceWrapper = document.querySelector('.product-price');
     priceWrapper.appendChild(document.createElement('br'));
     priceWrapper.appendChild(document.createElement('br'));
     priceWrapper.appendChild(button);
 
     button.addEventListener('click', function() {
-        data = {}
-        data['id'] = parseInt(window.location.pathname.match(/\d+/))
-        const urlParams = new URLSearchParams(window.location.search);
+        let data = {}
+        data['id'] = window.location.pathname.match(/\d+/)[0];
+        let urlParams = new URLSearchParams(window.location.search);
         if (urlParams.has('sku_id')) {
           console.log(encodeURIComponent(urlParams.get('sku_id')));
-          data['skuid'] = parseInt(encodeURIComponent(urlParams.get('sku_id')));
+          data['skuid'] = encodeURIComponent(urlParams.get('sku_id'));
         }
         else {
           alert('Выбраны не все параметры!');
-          return 0;
+          return;
         }
         data['count'] = parseInt(document.querySelector('.product-number-picker input').value);
 
